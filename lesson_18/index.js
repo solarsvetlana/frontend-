26152d96d16b4f6ec05964b79792b16353f67397
solarsvetlana <ruslanasolar50@gmail.com>
2025-08-31 22:33:06 +0200

    home work

--- lesson_18/index.js
+++ lesson_18/index.js
@@ -21,3 +21,29 @@ let ann ={
 }
 console.log(ann);
 console.log(ann.info());
+
+
+
+const item1 = {
+    name: 'notebook lenovo thinkpad',
+    price: 1283,
+    description:'cpu intel core7, ram:16gb', 
+    info: function(){
+        return `товар: ${this.name}; цена: ${this.price} описание: ${this.description}`;
+    }, 
+    note: {
+      title: "qwe",
+      num: 1900,
+      arr: ["qwe",1]  
+    }
+
+}
+console.log(item1);
+
+
+const itemJSON = JSON.stringify(item1);  // object -> json
+console.log(itemJSON);
+
+
+let obj = JSON.parse(itemJSON);  // json -> object
+    console.log(obj);
--- lesson_18/index1.js
+++ lesson_18/index1.js
@@ -1,4 +1,15 @@
 console.log("1");
-setTimeout(()=> console.log("2"), 2000)
-console.log("3");
-
+setTimeout(() => console.log("2"), 3000);
+//for(let i = 0; i<100000; i++){let a=10/2}
+setTimeout(() => console.log("3"), 1000);
+console.log("4");
+console.log("-------------------------------------------"); 
+setTimeout(() => {
+  console.log(1);
+  setTimeout(() => {
+    console.log(2);
+    setTimeout(() => {
+      console.log(3);
+    }, 5000);
+  }, 1000);
+}, 3000);
